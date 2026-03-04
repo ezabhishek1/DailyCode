359add00d6c876d5e9d3612c2acba5b6c8a060a9
Abhishek Gupta <143184381+ezabhishek1@users.noreply.github.com>
2026-03-04 07:55:44 +0000

    adding different approch too

--- MEDIUM/ Max Xor Subarray of size K/solution.cpp
+++ MEDIUM/ Max Xor Subarray of size K/solution.cpp
@@ -1,5 +1,6 @@
 class Solution {
   public:
+    // Approach 1: Sliding Window with Index
     int maxSubarrayXOR(vector<int>& arr, int k) {
         int n = arr.size();
         
@@ -20,6 +21,29 @@ class Solution {
             maxXor = max(maxXor, currentXor);
         }
         
+        return maxXor;
+    }
+    
+    // Approach 2: Two Pointer Logic
+    int maxSubarrayXORTwoPointer(vector<int>& arr, int k) {
+        int n = arr.size();
+        int maxXor = INT_MIN;
+        int currentXor = arr[0];
+        
+        int l = 0, r = 1;
+        while(l <= n - k) {
+            if(r - l + 1 > k) {
+                maxXor = max(maxXor, currentXor);
+                currentXor ^= arr[l];
+                l++;
+            }
+            currentXor ^= arr[r];
+            r++;
+        }
+        
+        if(maxXor == INT_MIN) {
+            return currentXor;
+        }
         return maxXor;
     }
 };
--- MEDIUM/ Max Xor Subarray of size K/solution.py
+++ MEDIUM/ Max Xor Subarray of size K/solution.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Approach 1: Sliding Window with Index
     def maxSubarrayXOR(self, arr, k):
         n = len(arr)
         
@@ -17,4 +18,24 @@ class Solution:
             current_xor ^= arr[i]
             max_xor = max(max_xor, current_xor)
         
+        return max_xor
+    
+    # Approach 2: Two Pointer Logic
+    def maxSubarrayXORTwoPointer(self, arr, k):
+        n = len(arr)
+        max_xor = float('-inf')
+        current_xor = arr[0]
+        
+        l = 0
+        r = 1
+        while l <= n - k:
+            if r - l + 1 > k:
+                max_xor = max(max_xor, current_xor)
+                current_xor ^= arr[l]
+                l += 1
+            current_xor ^= arr[r]
+            r += 1
+        
+        if max_xor == float('-inf'):
+            return current_xor
         return max_xor
