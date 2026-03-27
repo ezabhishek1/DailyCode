ba4f2c1f710d614495524784cbc08439a4763d54
Abhishek Gupta <143184381+ezabhishek1@users.noreply.github.com>
2026-03-27 04:17:23 +0000

    cp version

--- HARD/Chocolates Pickup/solution.cpp
+++ HARD/Chocolates Pickup/solution.cpp
@@ -1,33 +1,36 @@
 class Solution {
   public:
-    virtual int maxChocolateService(int i, int j1, int j2, int &n, int &m, vector<vector<int>> &grid,
-     vector<vector<vector<int>>> &dp){
-       if(j1<0 || j2<0 || j1>=m || j2>=m) return -(int)1e9;
-       
-       if(i==n-1){
-         if(j1==j2) return grid[i][j1];
-         return (grid[i][j1] + grid[i][j2]);  
-       }
-       
-       if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
-       
-       int maxi=INT_MIN;
-       
-       for(int dj1=-1; dj1<2; dj1++){
-         for(int dj2=-1; dj2<2; dj2++){
-           if(j1==j2) maxi=max(maxi, grid[i][j1]+maxChocolateService(i+1, j1+dj1, j2+dj2, n, m, grid, dp));
-           else maxi=max(maxi, grid[i][j1]+grid[i][j2]+maxChocolateService(i+1, j1+dj1, j2+dj2, n, m, grid, dp));
-         }   
-       }
-       
-       return dp[i][j1][j2]=maxi;
-     }
-    
-    virtual int maxChocolate(vector<vector<int>>& grid){
-      int n=grid.size(), m=grid[0].size();
-      
-      vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(m+1, vector<int>(m+1, -1)));
-      return maxChocolateService(0, 0, m-1, n, m, grid, dp);
+    int solve(int n, int m, vector<vector<int>>& grid) {
+        vector<vector<int>> front(m, vector<int>(m, 0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                if(i==j) front[i][j]=grid[n-1][i];
+                else front[i][j]=grid[n-1][i]+grid[n-1][j];
+            }
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            vector<vector<int>> cur(m, vector<int>(m, 0));
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+                    int maxi=-1e8;
+                    for(int di=-1;di<=1;di++){
+                        for(int dj=-1;dj<=1;dj++){
+                            if(j1+di<0 || j2+dj<0 || j1+di>=m || j2+dj>=m) continue;
+                            int val=0;
+                            if(j1==j2) val=grid[i][j1];
+                            else val=grid[i][j2]+grid[i][j1];
+                            val+=front[j1+di][j2+dj];
+                            maxi=max(maxi,val);
+                        }
+                    }
+                    cur[j1][j2]=maxi;
+                }
+            }
+            front=cur;
+        }
+
+       return front[0][m - 1];
+        
     }
 };
-
