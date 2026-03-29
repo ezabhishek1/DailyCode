25b3836cf3a5ab5421d01e13982ddd0a64c950ef
Abhishek Gupta <143184381+ezabhishek1@users.noreply.github.com>
2026-03-29 16:23:18 +0000

    py version

---
+++ MEDIUM/Partitions with Given Difference/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+      def countPartitions(self, arr, diff):
+     
+        s = sum(arr)
+        n = len(arr)
+        if (s-diff)&1 != 0 or s < diff:
+            return 0
+        s = (s-diff)//2
+ 
+        dp = [[0]*(n+1) for _ in range(s+1)]
+      
+        dp[0][0] = 1
+            
+        
+        for k in range(0, s+1): 
+            for i in range(n):
+                dp[k][i+1] = dp[k][i]
+                if k-arr[i] >= 0:
+                    dp[k][i+1] += dp[k-arr[i]][i]
+
+        return dp[-1][-1]
