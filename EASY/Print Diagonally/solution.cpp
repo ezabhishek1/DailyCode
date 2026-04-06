747dfca4269f724eb2b1f61526ec0811ee81e020
Abhishek Gupta <143184381+ezabhishek1@users.noreply.github.com>
2026-04-06 09:47:25 +0000

    cpp version of the solution for the problem "Print Diagonally" in the EASY category. The file is located at EASY/Print Diagonally/solution.cpp.

---
+++ EASY/Print Diagonally/solution.cpp
@@ -0,0 +1,34 @@
+class Solution 
+{
+  public:
+    vector<int> diagView(vector<vector<int>> mat) 
+    {
+        // Sam
+        int n = mat.size();
+        vector<int> result;
+        for(int i = 0;i<n;i++)
+        {
+            int j = i;
+            int k = 0;
+            while(j>=0 && k<n)
+            {
+                result.push_back(mat[k][j]);
+                j--;
+                k++;
+            }
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+            int j = n-1;
+            int k = i;
+            while(j>=0 && k<n)
+            {
+                result.push_back(mat[k][j]);
+                k++;
+                j--;
+            }
+        }
+        return result;
+    }
+};
