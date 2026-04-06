67bc47f7fb6026a71f63e05bd3b37a32ce55ccbf
Abhishek Gupta <143184381+ezabhishek1@users.noreply.github.com>
2026-04-06 09:48:39 +0000

    cpp version for Gray Code problem

---
+++ MEDIUM/Gray Code/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<string> graycode(int n) {
+        vector<string> result;
+        
+        int total = 1 << n;
+        
+        for (int i = 0; i < total; i++) {
+            
+            int gray = i ^ (i >> 1);
+            
+            string binary = "";
+            
+            for (int bit = n - 1; bit >= 0; bit--) {
+                if (gray & (1 << bit))
+                    binary += '1';
+                else
+                    binary += '0';
+            }
+            
+            result.push_back(binary);
+        }
+        
+        return result;
+    }
+};
