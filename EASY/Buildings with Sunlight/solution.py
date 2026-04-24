5e3a0b2ccb6c16d0b858e21ed82641c1bef8282d
Abhishek Gupta <143184381+ezabhishek1@users.noreply.github.com>
2026-04-24 10:43:00 +0000

    py version code

---
+++ EASY/Buildings with Sunlight/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def visibleBuildings(self, arr):
+        # code here
+        count = 0
+        max_height = 0
+        
+        for height in arr:
+            if (height > max_height or height == max_height):
+                count += 1
+                max_height = height
+        
+        return count
