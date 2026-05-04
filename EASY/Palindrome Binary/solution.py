645968f098cc1276439df3635b1b742ee821fb0e
Abhishek Gupta <143184381+ezabhishek1@users.noreply.github.com>
2026-05-04 14:21:15 +0000

    py version

---
+++ EASY/Palindrome Binary/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isBinaryPalindrome(self, n):
+        # code here
+        
+        binaryvalue=[]
+        while n>0:
+            binaryvalue.append(n%2)
+            n=n//2
+        
+        m=len(binaryvalue)
+        left=0
+        right=m-1
+        
+        while left<right:
+            if binaryvalue[left]!=binaryvalue[right]:
+                return False
+            left=left+1
+            right=right-1
+        return True
+        
