795be924af36b39adcc77bd88c967fbc85934faa
Abhishek Gupta <143184381+ezabhishek1@users.noreply.github.com>
2026-05-12 17:01:44 +0000

    py version

---
+++ HARD/Palindrome Pairs/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def palindromePair(self, a: list) -> bool:
+        """Return True if `a` contains two words which connect to form a palindrome."""
+
+        # Build the set of words.
+        words = set()
+
+        for word in a:
+            # Watch for palindromic copies.
+            if word in words:
+                return True
+            words.add(word[::-1])
+        
+        # Iterate through prefixes and suffixes.
+        for word in a:
+            j = 1
+            for _ in word[:-1]:
+                p, s = word[:j], word[j:]
+                if p in words and s == s[::-1] or s in words and p == p[::-1]:
+                    return True
+                j+=1
+
+        return False
+        
